96ced8f849f27a00b46aa0223a6174e1bf64105c
Zokoxa <zokoxa@outlook.com>
2023-09-14 20:16:34 -0700

    Upload from local... Urbby Clone Code.

---
+++ bot.py
@@ -0,0 +1,54 @@
+import responses
+import discord
+from discord.ext import tasks
+
+token = 'MTA4MTI1MTU3NjM1NTc1ODE4MA.G7Z75N.u-McQDY5jIeqK0ja5Yqd3mP719mOOkrWbWV-3A'
+
+client = discord.Client(intents=discord.Intents.all())
+
+urbanDURL = "https://www.urbandictionary.com/"
+
+channels = []
+# 1081429445300207617
+
+
+@tasks.loop(hours=24)
+async def send_message():
+
+    word_of_the_day = responses.get_word_of_day()
+
+    for channel in channels:
+        this_channel = client.get_channel(channel)
+        await this_channel.send(responses.handle_word_of_the_day(word_of_the_day))
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Urban Dictionary"))
+    send_message.start()
+    print('Urbby Clone Bot is running')
+
+
+@client.event
+async def on_message(mes):
+    if mes.author == client.user:
+        return
+
+    message = mes
+    # add channel id
+    if message.content == "!zSET":
+        channel_id = message.channel.id
+        if channels.__contains__(channel_id):
+            await message.channel.send("`Channel Already Registered!`")
+            return
+        channels.append(channel_id)
+        word_of_the_day = responses.get_word_of_day()
+        await message.channel.send(responses.handle_word_of_the_day(word_of_the_day))
+        await message.channel.send("`Channel Registered!`")
+        # remove channel id
+    if message.content == "!zREM":
+        channel_id = message.channel.id
+        channels.remove(channel_id)
+        await message.channel.send("`Channel Removed!`")
+
+client.run(token)
---
+++ responses.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def handle_word_of_the_day(data) -> str:
+    first_definition = data["list"][0]  # gets first definition
+    author = first_definition["author"]  # author of definition
+    definition = first_definition["definition"]  # definition of word
+    word = first_definition["word"]
+    definition = definition.replace('[', '')
+    definition = definition.replace(']', '')
+    return "`Word of the Day: " + word + " \nDefinition: " + definition + "`"
+
+
+def get_word_of_day():
+    res = requests.get("http://api.urbandictionary.com/v0/define?term=" + scrape_word())
+    return res.json()
+
+
+# scraping from urban dictionary
+def scrape_word() -> str:
+    req = requests.get("https://www.urbandictionary.com/")
+    soup = BeautifulSoup(req.text, "html.parser")
+    tag = soup.find('h1')
+    word = tag.string
+    return word
